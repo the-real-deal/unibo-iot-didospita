d19b1b1cddee0baf86b3c1a312f2a328c3da9118
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-08 10:40:26 +0200

    flake devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Generic nix devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # nix
+              nixd
+              nil
+              nixfmt
+            ];
+          };
+      }
+    );
+}
