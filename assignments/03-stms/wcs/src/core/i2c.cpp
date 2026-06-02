25d5e14a9292855dff22ce20c7ac92757e659611
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-02 17:59:57 +0200

    feat: add i2c manager

---
+++ assignments/03-stms/wcs/src/core/i2c.cpp
@@ -0,0 +1,31 @@
+#include "i2c.hpp"
+
+#include <Arduino.h>
+#include <Wire.h>
+
+I2CManager::I2CManager() : startAddress(I2C_MIN_ADDRESS) {}
+
+void I2CManager::setup()
+{
+  Wire.begin();
+}
+
+uint8_t I2CManager::scan()
+{
+  for (uint8_t address = this->startAddress; address <= I2C_MAX_ADDRESS;
+       address++)
+  {
+    Wire.beginTransmission(address);
+    const uint8_t error = Wire.endTransmission();
+    if (error == 0)
+    {
+      this->startAddress = address + 1;
+      return address;
+    }
+    else if (error == 4)
+    {
+      exit(error);
+    }
+  }
+  exit(-1);
+}
---
+++ assignments/03-stms/wcs/src/core/i2c.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "setup.hpp"
+
+#ifndef I2C_MIN_ADDRESS
+#define I2C_MIN_ADDRESS 0
+#endif
+
+#ifndef I2C_MAX_ADDRESS
+#define I2C_MAX_ADDRESS 126
+#endif
+
+class I2CManager : public Setup
+{
+private:
+  uint8_t startAddress;
+
+public:
+  void setup() override;
+  I2CManager();
+  uint8_t scan();
+};
