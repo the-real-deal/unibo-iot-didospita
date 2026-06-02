9124374bb498b40b3c1dae0e16c1364d34cd996c
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-02 18:07:51 +0200

    feat: add std dir

---
+++ assignments/03-stms/wcs/src/std/debug.cpp
@@ -0,0 +1,17 @@
+#include "debug.hpp"
+
+#include <Arduino.h>
+
+// https://docs.arduino.cc/learn/programming/memory-guide/#sram-memory-measurement
+
+void display_freeram() {
+  Serial.print(F("- SRAM left: "));
+  Serial.println(freeRam());
+}
+
+int freeRam() {
+  extern int __heap_start,*__brkval;
+  int v;
+  return (int)&v - (__brkval == 0  
+    ? (int)&__heap_start : (int) __brkval);  
+}
