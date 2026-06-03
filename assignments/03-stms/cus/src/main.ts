f01e12c7564564ed17b2f9601592d7a82d59d5dd
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-03 09:20:34 +0200

    feat: add template express app

--- assignments/03-stms/cus/src/main.ts
+++ assignments/03-stms/cus/src/main.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!")
+import { env } from "bun"
+import express from "express"
+
+const PORT = env.CUS_PORT || 3000
+
+const app = express()
+
+app.get("/{:name}", (req, res) => {
+  const name = req.params.name || "world"
+  res.send(`Hello, ${name}!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port http://localhost:${PORT}`)
+})
