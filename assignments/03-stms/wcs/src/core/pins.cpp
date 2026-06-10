391a1df278313e7a9b4ee76ed736e726770fb973
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-10 14:46:15 +0200

    feat: interrupt pin

--- assignments/03-stms/wcs/src/core/pins.cpp
+++ assignments/03-stms/wcs/src/core/pins.cpp
@@ -2,22 +2,22 @@
 
 #include <assert.h>
 
-BasePin::BasePin(uint8_t pin, PinType type) : pin(pin), type(type) {}
+IOPin::IOPin(uint8_t pin, PinType type) : pin(pin), type(type) {}
 
-void BasePin::setup()
+void IOPin::setup()
 {
   pinMode(this->pin, static_cast<uint8_t>(type));
 }
 
 DigitalOutputPin::DigitalOutputPin(uint8_t pin)
-    : BasePin(pin, PinType::Output) {}
+    : IOPin(pin, PinType::Output) {}
 
 void DigitalOutputPin::write(DigitalValue value)
 {
   digitalWrite(this->pin, static_cast<uint8_t>(value));
 }
 
-DigitalInputPin::DigitalInputPin(uint8_t pin) : BasePin(pin, PinType::Input) {}
+DigitalInputPin::DigitalInputPin(uint8_t pin) : IOPin(pin, PinType::Input) {}
 
 DigitalValue DigitalInputPin::read()
 {
@@ -34,14 +34,37 @@ uint32_t DigitalInputPin::readPulse(DigitalValue value, uint32_t timeout)
   return pulseIn(this->pin, static_cast<uint8_t>(value), timeout);
 }
 
-AnalogOutputPin::AnalogOutputPin(uint8_t pin) : BasePin(pin, PinType::Output) {}
+AnalogOutputPin::AnalogOutputPin(uint8_t pin) : IOPin(pin, PinType::Output) {}
 
 void AnalogOutputPin::write(uint8_t value) { analogWrite(this->pin, value); }
 
-AnalogInputPin::AnalogInputPin(uint8_t pin) : BasePin(pin, PinType::Input) {}
+AnalogInputPin::AnalogInputPin(uint8_t pin) : IOPin(pin, PinType::Input) {}
 
 double AnalogInputPin::read()
 {
   return static_cast<double>(analogRead(this->pin)) /
          static_cast<double>(AnalogInputPin::MAX_READ_VALUE);
+}
+
+InterruptPin::InterruptPin(uint8_t pin, InterruptMode mode, interruptFunctionType handler)
+    : pin(pin), mode(mode), handler(handler) {}
+
+void InterruptPin::setup()
+{
+  enableInterrupt(this->pin, this->handler, static_cast<uint8_t>(this->mode));
+}
+
+uint8_t InterruptPin::getPin()
+{
+  return this->pin;
+}
+
+InterruptMode InterruptPin::getMode()
+{
+  return this->mode;
+}
+
+interruptFunctionType InterruptPin::getHandler()
+{
+  return this->handler;
 }
--- assignments/03-stms/wcs/src/core/pins.hpp
+++ assignments/03-stms/wcs/src/core/pins.hpp
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 #include <stdint.h>
+#include <EnableInterrupt.h>
 
 #include "setup.hpp"
 
@@ -11,12 +12,12 @@ enum class PinType : uint8_t
   Output = OUTPUT,
 };
 
-class BasePin : public Setup
+class IOPin : public Setup
 {
 protected:
   uint8_t pin;
   PinType type;
-  BasePin(uint8_t pin, PinType type);
+  IOPin(uint8_t pin, PinType type);
 
 public:
   void setup() override;
@@ -30,14 +31,14 @@ enum class DigitalValue : uint8_t
   Low = LOW,
 };
 
-class DigitalOutputPin : public BasePin
+class DigitalOutputPin : public IOPin
 {
 public:
   DigitalOutputPin(uint8_t pin);
   void write(DigitalValue status);
 };
 
-class DigitalInputPin : public BasePin
+class DigitalInputPin : public IOPin
 {
 public:
   DigitalInputPin(uint8_t pin);
@@ -46,14 +47,14 @@ public:
   uint32_t readPulse(DigitalValue value, uint32_t timeout);
 };
 
-class AnalogOutputPin : public BasePin
+class AnalogOutputPin : public IOPin
 {
 public:
   AnalogOutputPin(uint8_t pin);
   void write(uint8_t value);
 };
 
-class AnalogInputPin : public BasePin
+class AnalogInputPin : public IOPin
 {
 private:
   static const uint32_t MAX_READ_VALUE = 1023;
@@ -61,4 +62,28 @@ private:
 public:
   AnalogInputPin(uint8_t pin);
   double read();
+};
+
+enum class InterruptMode : uint8_t
+{
+  Rising = RISING,
+  Falling = FALLING,
+  Change = CHANGE,
+  High = HIGH,
+  Low = LOW,
+};
+
+class InterruptPin : public Setup
+{
+private:
+  uint8_t pin;
+  InterruptMode mode;
+  interruptFunctionType handler;
+
+public:
+  InterruptPin(uint8_t pin, InterruptMode mode, interruptFunctionType handler);
+  virtual void setup() override;
+  uint8_t getPin();
+  InterruptMode getMode();
+  interruptFunctionType getHandler();
 };
